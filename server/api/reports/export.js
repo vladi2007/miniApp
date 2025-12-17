c84f8108ff93587b46ae9f8dd5110ed4c4382fc8
vladi2007 <ikonev05@mail.ru>
2025-12-17 19:16:14 +0500

    fix bug with uncorrect open report

--- server/api/reports/export.js
+++ server/api/reports/export.js
@@ -1,5 +1,4 @@
-import { writeFile, mkdir } from 'fs/promises'
-import { join } from 'path'
+
 
 export default defineEventHandler(async (event) => {
   try {
@@ -8,44 +7,16 @@ export default defineEventHandler(async (event) => {
     const response = await fetch('https://carclicker.ru/api/reports/export?x_key=super-secret-key', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/octet-stream'
-      },
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
       body: JSON.stringify(body)
     })
 
-    if (!response.ok) {
-      const errorText = await response.text()
-      throw new Error(`Ошибка внешнего API: ${response.status} - ${errorText}`)
-    }
+   const data = await response.json()
+   console.log(response)
+    return {data: data.url, name:data.name}
     
-    // Получаем имя файла из заголовка Content-Disposition
-    const contentDisposition = response.headers.get('content-disposition') || ''
-    const match = contentDisposition.match(/filename="?([^"]+)"?/)
-    const hostFilename = `report-${Date.now()}.xlsx`
-   
-    const fileName = match ? match[1] : `report-${Date.now()}.xlsx`
-     console.log(fileName)
-    const arrayBuffer = await response.arrayBuffer()
-    const buffer = Buffer.from(arrayBuffer)
-
-    const reportsDir = join(process.cwd(), 'public', 'reports')
-
-    // Создаем папку, если её нет
-    await mkdir(reportsDir, { recursive: true })
-
-    // Генерируем имя файла
-    
-    const filePath = join(reportsDir, hostFilename)
-
-    // Записываем файл
-    await writeFile(filePath, buffer)
-    
-    // Возвращаем путь к файлу относительно public, чтобы фронтенд мог скачать
-    return {
-     url: `/reports/${hostFilename}`,
-     userFileName: fileName
-    }
   } catch (error) {
     return {
       success: false,
