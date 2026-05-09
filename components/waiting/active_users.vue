601ade60735c1529dc4578ea98b5a42626e048b7
vladi2007 <ikonev05@mail.ru>
2026-05-09 18:45:30 +0500

    add active count to waiting leader

--- components/waiting/active_users.vue
+++ components/waiting/active_users.vue
@@ -9,7 +9,7 @@ const props = defineProps<{ count: string, context: string }>()
       {{ count }}
     </div>
     <div :class="$style.active__text">
-      Активные<br> пользователи
+      Подключившихся<br> участников
     </div>
   </div>
 </template>
@@ -33,13 +33,33 @@ const props = defineProps<{ count: string, context: string }>()
   font-size: 24px;
   font-weight: 500;
 
-
+  @media (min-width:1280px) {
+    justify-content: center;
+    margin-top: calc(59/832*100dvh);
+    height: calc(97/832*100dvh);
+    gap: calc(12/1280*100dvw);
+    ;
+  }
 
   &__count {
 
     padding: 7px 5px 7px 5px;
     background-color: #F7F7F766;
     border-radius: 5px;
+
+    @media (min-width:1280px) {
+      height: calc(97/832*100dvh);
+      aspect-ratio: 1;
+      border: 2px solid #853CFF;
+      border-radius: 50%;
+      ;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: #1D1D1D;
+      font-size: clamp(40px, 2.5vw, 56px);
+      ;
+    }
   }
 
   &__text {
@@ -52,8 +72,25 @@ const props = defineProps<{ count: string, context: string }>()
     background-color: #F7F7F766;
     border-radius: 5px;
 
+    @media (min-width:1280px) {
+      padding: 0;
+      height: calc(61/832*100dvh);
+      width: calc(280/1280*100dvw);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+      font-size: clamp(32px, 2vw, 44px);
+      color: #1D1D1D;
+      background-color: white;
+    }
+
     &>br {
       display: none;
+
+      @media (min-width:1280px) {
+        display: flex;
+      }
     }
   }
 }
